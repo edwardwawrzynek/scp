6047a4205aebe6225ac8a30c7eac14e15b6d1d8b
darksteelcode <32946898+darksteelcode@users.noreply.github.com>
2018-05-13 15:48:04 -0600

    microcode assembley works with non-symbolic values

--- microcode/microcode_asm.py
+++ microcode/microcode_asm.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 import sys
 
+#Number of signals, in bits
+SIG_BITS = 44
+#Number of entries in the microcode rom
+NUM_ENTRIES = 1024
+
 def getSigsFromFile(f):
     #signal name definitions
     sigs = []
@@ -24,5 +29,66 @@ def getSigsFromFile(f):
         sigs.append([name,start,end])
     return sigs
 
+def getStartAndEnd(name, sigs):
+    for s in sigs:
+        if s[0] == name:
+            return s[1],s[2]
+
+def proc_microcode(f, outf, sigs):
+    f = open(f)
+    #Result array
+    result = [[0]*SIG_BITS]*NUM_ENTRIES
+    for l in f:
+        #Output for this line- 0 by default
+        out = [0] * SIG_BITS
+        #Ignore comments
+        i = l.find("//")
+        if i!=-1:
+            l = l[:i]
+        #Replace whitespace
+        l = l.replace(" ", "")
+        l = l.replace("\n", "")
+        l = l.replace("\t", "")
+        #If line is empty, skip it
+        if l == "":
+            continue
+        #Break line into addr, name, and body
+        lSplit = l.split(":")
+        addr = int(lSplit[0])
+        name = lSplit[1]
+        body = lSplit[2]
+        #Split body into individual cmds
+        cmds = body.split(",")
+        #Change out for each cmd
+        for c in cmds:
+            #If no specific value is set, it is set to 1
+            val = 1
+            split = c.split('=')
+            #If the sig name is set to a value, set val to it
+            if len(split) > 1:
+                val = int(split[1])
+            c = split[0]
+            start,end = getStartAndEnd(c, sigs)
+            #amount to shift right to get each bit
+            shift = 0
+            pos = end
+            #iterate through each bit from start to end
+            while pos >= start:
+                out[pos] = (val>>shift)&1
+                pos-=1
+                shift += 1
+        result[addr] = out
+    mifFile = open(outf, 'w')
+    mifFile.write("WIDTH="+str(SIG_BITS)+";\nDEPTH="+str(NUM_ENTRIES)+";\nADDRESS_RADIX=UNS;\nDATA_RADIX=BIN;\nCONTENT BEGIN\n")
+    for r in range(NUM_ENTRIES):
+        mifFile.write("\t")
+        mifFile.write(str(r))
+        mifFile.write(" : ")
+        for b in result[r]:
+            mifFile.write(str(b))
+        mifFile.write(";\n")
+    mifFile.write("END;")
+    mifFile.close()
+
 sigs = getSigsFromFile("defs.txt")
-print sigs
+proc_microcode("microcode.txt", "microcode.mif", sigs)
