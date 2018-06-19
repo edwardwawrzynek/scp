5808b4a02a89a9fc88d66a589955e42dc4c1eb84
darksteelcode <32946898+darksteelcode@users.noreply.github.com>
2018-06-19 16:37:49 -0600

    Added keycode defs for odd keys

--- smallC/includes/stdio.h
+++ smallC/includes/stdio.h
@@ -4,3 +4,24 @@
 #define NULL 0
 #define EOF (-1)
 #define FILE char
+
+#define KEY_F1 11
+#define KEY_F2 12
+#define KEY_F3 13
+#define KEY_F4 14
+#define KEY_F5 15
+#define KEY_F6 19
+#define KEY_F7 20
+#define KEY_F8 21
+#define KEY_F9 22
+#define KEY_F10 23
+#define KEY_F11 24
+#define KEY_F12 25
+
+#define KEY_LEFT 28
+#define KEY_UP 29
+#define KEY_RIGHT 30
+#define KEY_DOWN 31
+
+#define KEY_PAGE_UP 33
+#define KEY_PAGE_DOWN 34
