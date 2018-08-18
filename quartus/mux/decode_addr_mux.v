3fbfebe82825e9a8e5db1f15b896970b23d73de2
edwardwawrzynek <32946898+darksteelcode@users.noreply.github.com>
2018-08-18 15:46:16 -0600

    misc

--- quartus/mux/decode_addr_mux.v
+++ quartus/mux/decode_addr_mux.v
@@ -2,9 +2,15 @@ module decode_addr_mux(
 	input [7:0] ir,
 	input [9:0] decode_next,
 	input sel,
-	output [9:0] addr
+	output [9:0] addr,
+	input int
 );
 
-assign addr = sel ? {2'b0,ir} : decode_next;
+parameter INT_ADDR = 1023;
+
+wire [9:0] norm_out;
+assign norm_out = sel ? {2'b0,ir} : decode_next;
+
+assign addr = (sel && int) ? INT_ADDR : norm_out;
 
 endmodule
