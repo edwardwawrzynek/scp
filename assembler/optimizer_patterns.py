4e8b8b95ac89b2d9544f540bf9842e0dab09d2d8
edwardwawrzynek <32946898+edwardwawrzynek@users.noreply.github.com>
2018-08-21 14:55:51 -0600

    Added new optomizer patterns

--- assembler/optimizer_patterns.py
+++ assembler/optimizer_patterns.py
@@ -4,13 +4,6 @@ from optimizer import *
 A = -1
 NO_ARG = arg("")
 patterns = [
-    #optomize lwi_ for values less than 256 as lbi_
-    [ [
-        ["lwia", arg.TYPE_LIT, lambda v: (v < 256 and v >= 0)], ],
-        lambda c:[cg("lbia", c[0].arg)], ],
-    [ [
-        ["lwib", arg.TYPE_LIT, lambda v: (v < 256 and v >= 0)], ],
-        lambda c:[cg("lbib", c[0].arg)], ],
     #optimize repetitive mdsp as one
     [ [
         ["mdsp", arg.TYPE_LIT, A],
@@ -19,19 +12,26 @@ patterns = [
     #uneeded psh and pop
     [ [
         ["psha", A, A],
-        ["lbia", A, A],
+        ["lwia,lbia", A, A],
         ["popb", A, A], ],
         lambda c:[cg("xswp", arg("")), c[1],], ],
     #pointer arithmetic constant multiplication
     [ [
-        ["lbia", arg.TYPE_LIT, A],
-        ["lbib", arg.TYPE_LIT, A],
+        ["lwia,lbia", arg.TYPE_LIT, A],
+        ["lwib,lbib", arg.TYPE_LIT, A],
         ["amul", A, A] ],
         lambda c:[cg("lwia", arg("#" + str(c[0].arg.val * c[1].arg.val)))] ],
     #optimize one byte mspa
     [ [
         ["mspa", arg.TYPE_LIT, lambda v: (v < 256 and v >= 0)] ],
         lambda c : [cg("bspa", c[0].arg)] ],
+    #optomize lwi_ for values less than 256 as lbi_
+    [ [
+        ["lwia", arg.TYPE_LIT, lambda v: (v < 256 and v >= 0)], ],
+        lambda c:[cg("lbia", c[0].arg)], ],
+    [ [
+        ["lwib", arg.TYPE_LIT, lambda v: (v < 256 and v >= 0)], ],
+        lambda c:[cg("lbib", c[0].arg)], ],
 ]
 
 main(patterns)
