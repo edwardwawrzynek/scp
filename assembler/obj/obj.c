b98476b815d8f2fa18cf335eecef17b0f5d0dcad
edwardwawrzynek <32946898+edwardwawrzynek@users.noreply.github.com>
2018-12-01 19:31:41 -0700

    add obj header creation support

--- assembler/obj/obj.c
+++ assembler/obj/obj.c
@@ -25,14 +25,27 @@ size| Description
 4   | Magic Number-SOF (SCP Object File) followed by null
 4   | Unused
 4   | Offset of segment 0 in file
+4   | Size of segment 0 in file
 4   | Offset of segment 1 in file
+4   | Size of segment 1 in file
 4   | Offset of segment 2 in file
+4   | Size of segment 2 in file
 4   | Offset of segment 3 in file
+4   | Size of segment 3 in file
 4   | Offset of defined symbol table
+4   | Size of defined symbol table
 4   | Offset of external symbol table
+4   | Size of external symbol table
 
 NOTE: all offsets are from the start of the file, including the 32 byte header
 
+
+*** Table Formats ***
+Both tables use the same format (in external table, additional entries beyond the name are blank)
+Size            |Description
+_OBJ_SYMBOL_SIZE|The symbol's name, including null
+1               |Which segment the symbol is in (defined symbol table only)
+2               |The symbol's offset in its segment (defined symbol table only)
 */
 
 #include <stdio.h>
@@ -45,9 +58,15 @@ NOTE: all offsets are from the start of the file, including the 32 byte header
 /* Size, including nulls, of symbol names (defined and external) - all symbol names will take up this much space
 * Note: if we change this, all object files will have to be regenerated */
 #define _OBJ_SYMBOL_SIZE 64
+/* Symbol table entries */
+struct obj_symbol_entry {
+  char name[_OBJ_SYMBOL_SIZE];
+  uint8_t seg;
+  uint16_t offset;
+};
 
 /* Header size in bytes (this is fixed unless we change header format) */
-#define _OBJ_HEADER_SIZE 32
+#define _OBJ_HEADER_SIZE 56
 
 /* describes a segment of the file */
 struct obj_seg {
@@ -118,12 +137,115 @@ void obj_write_header(struct obj_file *obj){
   for(int i = 0; i < 4; i++){
     /* TODO: check that the offsets and sizes are valid when we init the obj_segs struct */
     _obj_write_val(obj->file, obj->segs.segs[i].offset, 4);
+    _obj_write_val(obj->file, obj->segs.segs[i].size, 4);
   }
   /* write out symbol definitions */
   _obj_write_val(obj->file, obj->segs.defined_table.offset, 4);
+  _obj_write_val(obj->file, obj->segs.defined_table.size, 4);
   _obj_write_val(obj->file, obj->segs.extern_table.offset, 4);
+  _obj_write_val(obj->file, obj->segs.extern_table.size, 4);
   /* make sure we wrote the right size header */
   if(ftell(obj->file) != _OBJ_HEADER_SIZE){
-    _obj_error("Internal Error: expected header size doesn't match written headers size\n");
+    _obj_error("Internal Error: expected header size doesn't match written header's size");
+  }
+}
+
+/**
+ * init an obj_segs object by reading in the header of the obj_file */
+void obj_read_header(struct obj_file *obj){
+  /* seek to header start */
+  fseek(obj->file, 0, SEEK_SET);
+  /* read magic number, and make sure it is right */
+  if(_obj_read_val(obj->file, 4) != _OBJ_MAGIC_NUMBER){
+    _obj_error("Expected Magic Number to start file");
+  }
+  /* read blank */
+  _obj_read_val(obj->file, 4);
+  /* read in segs */
+  uint32_t offset = 0;
+  uint32_t tmp;
+  for(int i = 0; i < 4; i++){
+    /* TODO: check that the offsets and sizes are valid when we init the obj_segs struct */
+    tmp = _obj_read_val(obj->file, 4);
+    /* check that the segment is in the right place */
+    if(offset != tmp){
+      _obj_error("segment misaligned");
+    }
+    obj->segs.segs[i].offset = tmp;
+    tmp = _obj_read_val(obj->file, 4);
+    /* inc offset */
+    offset += tmp;
+    obj->segs.segs[i].size = tmp;
+  }
+  /* read in symbol definitions */
+  tmp = _obj_read_val(obj->file, 4);
+  if(offset != tmp){
+      _obj_error("symbol segment misaligned");
+  }
+  obj->segs.defined_table.offset = tmp;
+  tmp = _obj_read_val(obj->file, 4);
+  offset += tmp;
+  obj->segs.defined_table.size = tmp;
+
+
+  tmp = _obj_read_val(obj->file, 4);
+  if(offset != tmp){
+      _obj_error("extern symbol segment misaligned");
+  }
+  obj->segs.extern_table.offset = tmp;
+  tmp = _obj_read_val(obj->file, 4);
+  offset += tmp;
+  obj->segs.extern_table.size = tmp;
+  /* make sure we read in the right size header */
+  if(ftell(obj->file) != _OBJ_HEADER_SIZE){
+    _obj_error("Internal Error: expected header size doesn't match read header's size");
+  }
+}
+
+/**
+ * create the header in an obj_segs object given the sizes of eahc segment and number of defined and extern symbols
+ * symbols are the number of symbols, NOT the size of the tables
+ * segments sizes are in bytes (including the meta-bytes) */
+void obj_create_header(struct obj_file *obj, uint32_t seg0, uint32_t seg1, uint32_t seg2, uint32_t seg3, uint32_t defined_symbols, uint32_t extern_symbols){
+  uint32_t offset = 0;
+
+  for(int i = 0; i < 4; i++){
+    obj->segs.segs[i].offset = offset;
+    switch(i){
+      case 0: obj->segs.segs[i].size = seg0; offset += seg0; break;
+      case 1: obj->segs.segs[i].size = seg1; offset += seg1; break;
+      case 2: obj->segs.segs[i].size = seg2; offset += seg2; break;
+      case 3: obj->segs.segs[i].size = seg3; offset += seg3; break;
+      default: break;
+    }
+  }
+  obj->segs.defined_table.offset = offset;
+  obj->segs.defined_table.size = defined_symbols * sizeof(struct obj_symbol_entry);
+  offset += defined_symbols * sizeof(struct obj_symbol_entry);
+
+  obj->segs.extern_table.offset = offset;
+  obj->segs.extern_table.size = extern_symbols * sizeof(struct obj_symbol_entry);
+  offset += extern_symbols * sizeof(struct obj_symbol_entry);
+}
+
+void write(){
+  struct obj_file o;
+  o.file = fopen("out.txt", "w");
+  obj_create_header(&o, 10, 20, 30, 40, 5, 6);
+
+  obj_write_header(&o);
+}
+
+void read(){
+  struct obj_file o;
+  o.file = fopen("out.txt", "r");
+  obj_read_header(&o);
+}
+
+int main(int argc, char **argv){
+  if(argc > 1){
+    write();
+  } else {
+    read();
   }
 }
